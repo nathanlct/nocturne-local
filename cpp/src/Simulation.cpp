457ce9784cde20cd75f9a8a2a6aa16de9baa2610
nathanlct <nathan.lichtle@gmail.com>
2021-08-31 13:15:04 +0200

    Remove rendering in tests

--- cpp/src/Simulation.cpp
+++ cpp/src/Simulation.cpp
@@ -1,6 +1,5 @@
 #include "../include/nocturne_bits/Simulation.hpp"
 #include <iostream>
-#include <SFML/Graphics.hpp>
 
 Simulation::Simulation() {
         
@@ -8,23 +7,5 @@ Simulation::Simulation() {
 
 void Simulation::reset() const {
     std::cout << "Resetting simulation." << std::endl;
-
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
 }
 
