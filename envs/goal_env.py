9b0fab958f3cd96388beaa7cc56e2f8b8a597ba5
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2021-10-26 14:17:47 -0700

    add subscriber and sketch of env

---
+++ envs/goal_env.py
@@ -0,0 +1,56 @@
+"""Each agent receives its observation, a goal position, and tries to get there without colliding."""
+
+from gym import spaces
+import numpy as np
+
+from nocturne import Simulation
+from utils.subscribers import Subscriber
+
+class GoalEnv(object):
+    def __init__(self, scenario_path, cfg):
+        self.simulation = Simulation(scenario_path)
+        self.subscriber = Subscriber(cfg.subscriber)
+        self.cfg = cfg
+
+    def observation_space(self):
+        pass
+
+    def action_space(self):
+        pass
+
+    def step(self, action_dict):
+        obs_dict = {}
+        rew_dict = {}
+        done_dict = {'__all__': False}
+        info_dict = {}
+        rew_cfg = self.cfg.rew_cfg
+        for veh_id, veh_obj in self.simulation.getVehicles():
+            if veh_id in action_dict.keys():
+                action = action_dict[veh_id]
+                if 'accel' in action.keys():
+                    self.simulation.applyAccel(action['accel'])
+                if 'turn' in action.keys():
+                    self.simulation.applyTurn(action['turn'])
+                if 'tilt_view' in action.keys():
+                    self.simulation.applyViewTilt(action['tilt_view'])
+        self.simulation.step(self.cfg.dt)
+        for veh_id, veh_obj in self.simulation.getVehicles():
+            obs_dict[veh_id] = self.subscriber.get_obs(veh_obj)
+            rew_dict[veh_id] = 0
+            done_dict[veh_id] = False
+            if self.simulation.hasCollided(veh_obj):
+                rew_dict[veh_id] -= np.abs(rew_cfg.collision_penalty)
+                done_dict[veh_id] = True
+            if self.simulation.crossedLaneLines(veh_obj):
+                rew_dict[veh_id] -= np.abs(rew_cfg.crossed_lanes_penalty)
+            if self.simulation.goalAchieved(veh_obj):
+                rew_dict[veh_id] += np.abs(rew_cfg.goal_achieved_bonus)
+        
+        return obs_dict, rew_dict, done_dict, info_dict
+
+    def reset(self):
+        self.simulation.reset()
+        obs_dict = {}
+        for veh_id, veh_obj in self.simulation.getVehicles():
+            obs_dict[veh_id] = self.subscriber.get_obs(veh_obj)
+        return obs_dict
---
+++ utils/subscribers.py
@@ -0,0 +1,47 @@
+class Subscriber(object):
+    def __init__(self, cfg, simulation):
+        self.simulation = simulation
+        self.cfg = cfg
+        self.object_subscriber(cfg.object_subscriber_cfg, simulation)
+        self.ego_subscriber(cfg.ego_subscriber, simulation)
+
+    def get_obs(self, object):
+        obs_dict = {}
+        if self.cfg.include_ego_state:
+            obs_dict.update(self.ego_subscriber.get_obs(object))
+        if self.cfg.include_visible_object_state:
+            obs_dict['visible_objects'] = {obj_id: self.object_subscriber.get_obs(obj) for 
+                                           obj_id, obj in self.simulation.getVisibleObjects(object)}
+        return obs_dict 
+
+class ObjectSubscriber(object):
+    def __init__(self, cfg, simulation):
+        self.cfg = cfg
+        self.simulation = simulation
+
+    def get_obs(self, object):
+        obs_dict = {}
+        if self.cfg.include_speed:
+            obs_dict['curr_speed'] = self.simulation.getSpeed(object)
+        if self.cfg.include_xy:
+            obs_dict['xy'] = self.simulation.getXY(object)
+        return obs_dict
+
+class EgoSubscriber(object):
+    def __init__(self, cfg, simulation):
+        self.cfg = cfg
+        self.simulation = simulation
+    
+    def get_obs(self, object):
+        obs_dict = {}
+        if self.cfg.img_view:
+            obs_dict['ego_image'] = self.simulation.getCone(object, self.cfg.view_angle, object.getHeadTilt())
+        if self.cfg.include_speed:
+            obs_dict['ego_speed'] = self.simulation.getSpeed(object)
+        if self.cfg.include_goal_xy:
+            obs_dict['goal_xy'] = self.simulation.getGoalXY(object)
+        if self.cfg.include_goal_img:
+            obs_dict['goal_img'] = self.simulation.getGoalXY(object)
+        if self.cfg.include_lane_pos:
+            obs_dict['lane_pos'] = self.simulation.getLanePos(object)
+        return obs_dict
