d7c300da7c3d1c4c719546a9c644e4e906a9e451
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2021-11-03 07:27:15 -0700

    Minor, squash later

--- tests/test_rl_env.py
+++ tests/test_rl_env.py
@@ -1,6 +1,7 @@
 import os
 
 import hydra
+import matplotlib.pyplot as plt
 
 from envs.base_env import BaseEnv
 from utils.wrappers import create_env
@@ -9,12 +10,15 @@ os.environ["DISPLAY"] = ":0.0"
 
 @hydra.main(config_path='../cfgs/', config_name='config')
 def main(cfg):
+    cfg.scenario_path = '/private/home/eugenevinitsky/Code/nocturne/scenarios/two_car_intersection.json'
     env = create_env(cfg)
     obs = env.reset()
-    print(obs)
-    for i in range(10):
+    for _ in range(10):
         obs, rew, done, info = env.step({8: {'accel': 2.0, 'turn': 1.0}})
-        print(obs)
+    goal_img = obs[9]['goal_img']
+    plt.figure()
+    plt.imshow(goal_img)
+    plt.savefig('./goal_img.png')
 
 if __name__ == '__main__':
     main()
