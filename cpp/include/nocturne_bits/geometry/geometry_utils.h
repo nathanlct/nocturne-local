5733b516f758a7fa6003f8245a6d3e4f80406c10
Xiaomeng Yang <bit.yangxm@gmail.com>
2022-03-10 11:18:29 -0800

    Add AngleAdd and AngleSub in geometry_utils

--- cpp/include/nocturne_bits/geometry/geometry_utils.h
+++ cpp/include/nocturne_bits/geometry/geometry_utils.h
@@ -8,20 +8,48 @@ namespace utils {
 
 constexpr double kEps = 1e-8;
 constexpr double kPi = M_PI;
+constexpr double kTau = M_2_PI;
+constexpr double kTwoPi = M_2_PI;
+constexpr double kHalfPi = M_PI_2;
 
-template <typename T>
-inline bool AlmostEquals(const T& lhs, const T& rhs, const T& eps = kEps) {
+inline bool AlmostEquals(float lhs, float rhs, float eps = kEps) {
   return std::fabs(lhs - rhs) < eps;
 }
 
-template <typename T>
-constexpr T Radians(const T& d) {
-  return d / T(180.0) * static_cast<T>(kPi);
+inline bool AlmostEquals(double lhs, double rhs, double eps = kEps) {
+  return std::fabs(lhs - rhs) < eps;
 }
 
-template <typename T>
-constexpr T Degrees(const T& r) {
-  return r / static_cast<T>(kPi) * T(180.0);
+constexpr float Radians(float d) {
+  return d / 180.0f * static_cast<float>(kPi);
+}
+
+constexpr double Radians(double d) { return d / 180.0 * kPi; }
+
+constexpr float Degrees(float r) {
+  return r / static_cast<float>(kPi) * 180.0f;
+}
+
+constexpr double Degrees(double r) { return r / kPi * 180.0; }
+
+inline float AngleAdd(float lhs, float rhs) {
+  const float ret = fmod(lhs + rhs, static_cast<float>(kTau));
+  return ret < 0.0f ? ret + static_cast<float>(kTau) : ret;
+}
+
+inline double AngleAdd(double lhs, double rhs) {
+  const float ret = fmod(lhs + rhs, kTau);
+  return ret < 0.0 ? ret + kTau : ret;
+}
+
+inline float AngleSub(float lhs, float rhs) {
+  const float ret = fmod(lhs - rhs, static_cast<float>(kTau));
+  return ret < 0.0f ? ret + static_cast<float>(kTau) : ret;
+}
+
+inline double AngleSub(double lhs, double rhs) {
+  const float ret = fmod(lhs - rhs, kTau);
+  return ret < 0.0 ? ret + kTau : ret;
 }
 
 }  // namespace utils
