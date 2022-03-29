(Feature, bug): 1) remove double counting of stop signs as road points, 2) add scenario test script, 3) remove unnecessary test scripts

tests/test_state_subscribers.py
@@ -4,16 +4,17 @@ from pyvirtualdisplay import Display
 from nocturne import Simulation
 import time
 
-disp = Display()
-disp.start()
-sim = Simulation(scenario_path='./scenario_test.json')
+import os
+
+os.environ["DISPLAY"] = ":0.0"
+sim = Simulation(scenario_path='tests/scenario_test.json')
 scenario = sim.getScenario()
 vehs = scenario.getVehicles()
 
 ##################################
 # Test ego state getter
 #################################
-state = scenario.getEgoState(vehs[0])
+state = scenario.egoStateObservation(vehs[0])
 # speed, goal dist, goal angle, length, width
 np.testing.assert_allclose(state, [
     5.0 * np.sqrt(2), 100 * np.sqrt(2), 3 * np.pi / 4, vehs[0].getLength(),
@@ -25,62 +26,72 @@ np.testing.assert_allclose(vehs[0].getHeading(), np.pi / 2)
 ##################################
 # Test general state getter when we see every object
 #################################
-max_num_visible_objects = scenario.getMaxNumVisibleObjects()
-num_object_states = 6
+max_num_visible_objects = scenario.getMaxNumVisibleKineticObjects()
+num_object_states = scenario.getKineticObjectFeatureSize()
 max_num_visible_road_points = scenario.getMaxNumVisibleRoadPoints()
-num_road_point_states = 3
+num_road_point_states = scenario.getRoadPointFeatureSize()
 max_num_visible_stop_signs = scenario.getMaxNumVisibleStopSigns()
-num_stop_sign_states = 2
-max_num_visible_tl_signs = scenario.getMaxNumVisibleTLSigns()
-num_tl_states = 3
-new_state = scenario.getVisibleState(vehs[0], 2 * np.pi)
+num_stop_sign_states = scenario.getStopSignsFeatureSize()
+max_num_visible_tl_signs = scenario.getMaxNumVisibleTrafficLights()
+num_tl_states = scenario.getTrafficLightFeatureSize()
+new_state = scenario.observation(vehs[0], 120.0, 1.99 * np.pi)
 # check that the observed vehicle has the right state
 # the vehicle is 10 meters away northwards, pointed east, we are pointed north
-np.testing.assert_allclose(
-    new_state[0:num_object_states],
-    [10.0, 0.0, -np.pi / 2, 5.0, vehs[1].getLength(), vehs[1].getWidth()],
-    rtol=1e-5,
-    atol=1e-5)
-
-# check that the observed road points are fine they are at [(10, 10), (11, 11)]
-# and are "road edge = 3, road edge = 3"
-road_point_state = new_state[max_num_visible_objects *
-                             num_object_states:max_num_visible_objects *
-                             num_object_states + num_road_point_states * 2]
-np.testing.assert_allclose(
-    road_point_state,
-    [10.0 * np.sqrt(2), -np.pi / 4, 3, 11.0 * np.sqrt(2), -np.pi / 4, 3],
-    rtol=1e-5,
-    atol=1e-5)
-
-# now do the same thing with the stop sign at (8, 8)
-new_start_point = max_num_visible_objects * num_object_states + max_num_visible_road_points * num_road_point_states
-stop_sign_state = new_state[new_start_point:new_start_point +
-                            num_stop_sign_states]
-np.testing.assert_allclose(stop_sign_state, [8.0 * np.sqrt(2), -np.pi / 4],
+# so our result should be
+# [1 #valid, 10 #distance, 0 # azimuth, 2 # length, 1 # width, -pi/2 # relative-heading, 5 # speed]
+# the object is a vehicle so it gets one-hot to [0, 1, 0, 0, 0, 0, 0, 0]
+np.testing.assert_allclose(new_state[0:num_object_states], [
+    1, 10.0, 0.0, vehs[1].getLength(), vehs[1].getWidth(), -np.pi / 2, 5.0, 0,
+    1, 0, 0, 0, 0, 0, 0
+],
                            rtol=1e-5,
                            atol=1e-5)
 
-########################## now do the same but with a partially obscured view, we should only see the vehicle and nothing else
-new_state = scenario.getVisibleState(vehs[0], 0.1)
+# check that the observed road points are fine they are at [(10, 10), (11, 11)]
+# and are "road edge = 3, road edge = 3"
+# since these are road edges the one hot encoding will be [0, 0, 0, 1, 0, 0, 0]
+road_point_state = new_state[max_num_visible_objects *
+                             num_object_states:max_num_visible_objects *
+                             num_object_states + num_road_point_states * 2]
+np.testing.assert_allclose(road_point_state, [
+    1, 10.0 * np.sqrt(2), -np.pi / 4, 0, 0, 0, 1, 0, 0, 0, 1,
+    11.0 * np.sqrt(2), -np.pi / 4, 0, 0, 0, 1, 0, 0, 0
+],
+                           rtol=1e-5,
+                           atol=1e-5)
+
+# now do the same thing with the stop sign at (8, 8)
+new_start_point = max_num_visible_objects * num_object_states + max_num_visible_road_points * num_road_point_states + max_num_visible_tl_signs * num_tl_states
+stop_sign_state = new_state[new_start_point:new_start_point +
+                            num_stop_sign_states]
+np.testing.assert_allclose(stop_sign_state, [1, 8.0 * np.sqrt(2), -np.pi / 4],
+                           rtol=1e-5,
+                           atol=1e-5)
+
+########################## now do the sames but with a partially obscured view, we should only see the vehicle and nothing else
+new_state = scenario.observation(vehs[0], 120, 0.1)
 # vehicle
-np.testing.assert_allclose(
-    new_state[0:num_object_states],
-    [10.0, 0.0, -np.pi / 2, 5.0, vehs[1].getLength(), vehs[1].getWidth()],
-    rtol=1e-5,
-    atol=1e-5)
+np.testing.assert_allclose(new_state[0:num_object_states], [
+    1, 10.0, 0.0, vehs[1].getLength(), vehs[1].getWidth(), -np.pi / 2, 5.0, 0,
+    1, 0, 0, 0, 0, 0, 0
+],
+                           rtol=1e-5,
+                           atol=1e-5)
 # road point, it shouldn't be visible
 road_point_state = new_state[max_num_visible_objects *
                              num_object_states:max_num_visible_objects *
                              num_object_states + num_road_point_states * 2]
-np.testing.assert_allclose(road_point_state, [-100] * 6, rtol=1e-5, atol=1e-5)
+np.testing.assert_allclose(road_point_state, [0] * 2 * num_road_point_states,
+                           rtol=1e-5,
+                           atol=1e-5)
 
 ########################## now rotate the vehicle so it sees the road points but not the vehicle ##############
 vehs[0].setHeading(np.pi / 4)
-new_state = scenario.getVisibleState(vehs[0], 0.1)
+new_state = scenario.observation(vehs[0], 120, 0.1)
 print(vehs[0].getHeading())
 # vehicle
-np.testing.assert_allclose(new_state[0:num_object_states], [-100] * 6,
+np.testing.assert_allclose(new_state[0:num_object_states],
+                           [0] * num_object_states,
                            rtol=1e-5,
                            atol=1e-5)
 # check that the observed road points are fine they are at [(10, 10), (11, 11)]
@@ -88,8 +99,10 @@ np.testing.assert_allclose(new_state[0:num_object_states], [-100] * 6,
 road_point_state = new_state[max_num_visible_objects *
                              num_object_states:max_num_visible_objects *
                              num_object_states + num_road_point_states * 2]
-np.testing.assert_allclose(road_point_state,
-                           [10.0 * np.sqrt(2), 0, 3, 11.0 * np.sqrt(2), 0, 3],
+np.testing.assert_allclose(road_point_state, [
+    1, 10.0 * np.sqrt(2), 0, 0, 0, 0, 1, 0, 0, 0, 1, 11.0 * np.sqrt(2), 0, 0,
+    0, 0, 1, 0, 0, 0
+],
                            rtol=1e-5,
                            atol=1e-5)
 
@@ -100,7 +113,7 @@ img = np.array(scenario.getCone(scenario.getVehicles()[0], 2 * np.pi, 0.0,
 times = []
 for _ in range(5):
     t = time.time()
-    new_state = scenario.getVisibleState(vehs[0], 2 * np.pi)
+    new_state = scenario.observation(vehs[0], 120, 2 * np.pi)
     diff = time.time() - t
     times.append(diff)
 
@@ -109,7 +122,7 @@ print('run time for full view is is ', np.mean(times))
 times = []
 for _ in range(5):
     t = time.time()
-    new_state = scenario.getVisibleState(vehs[0], 1.58)
+    new_state = scenario.observation(vehs[0], 120, 1.58)
     diff = time.time() - t
     times.append(diff)
 