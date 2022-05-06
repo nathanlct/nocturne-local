757453bce4ffbc0990b4c42b3b2afd3851431715
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2022-05-06 09:50:33 -0700

    fix RL env test

--- tests/test_rl_env.py
+++ tests/test_rl_env.py
@@ -2,6 +2,7 @@ import os
 
 from hydra import compose, initialize
 from pathlib import Path
+from pyvirtualdisplay import Display
 
 from cfgs.config import PROJECT_PATH
 from nocturne_utils.wrappers import create_env
@@ -10,20 +11,25 @@ os.environ["DISPLAY"] = ":0.0"
 
 
 def test_rl_env():
+    disp = Display()
+    disp.start()
     initialize(config_path="../cfgs/")
     cfg = compose(config_name="config")
-    # move the scenario path to this folder
-    cfg.scenario_path = os.path.join(PROJECT_PATH, 'tests')
-    cfg.max_num_vehicles = 50
     env = create_env(cfg)
     env.files = [str(PROJECT_PATH / "tests/large_file.json")]
     obs = env.reset()
     # quick check that rendering works
     img = env.scenario.getCone(env.scenario.getVehicles()[0], 120.0,
                                1.99 * 3.14, 0.0, False)
-    img = env.render()
+    veh = env.scenario.getVehicles()[1]
+    veh_id = veh.getID()
     for _ in range(10):
-        obs, rew, done, info = env.step({8: {'accel': 2.0, 'turn': 1.0}})
+        prev_position = [veh.position.x, veh.position.y]
+        obs, rew, done, info = env.step({veh_id: {'accel': 2.0, 'turn': 1.0}})
+        if veh not in env.scenario.getVehicles():
+            break
+        new_position = [veh.position.x, veh.position.y]
+        assert prev_position != new_position, f'veh was in position {prev_position} which is the same as {new_position} but should have moved'
 
 
 if __name__ == '__main__':
