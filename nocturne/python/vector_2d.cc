7fa49bd4f71a26e9d32d588a3f4ca3c289a63c46
Xiaomeng Yang <bit.yangxm@gmail.com>
2022-05-19 10:55:34 -0700

    Add pickle for Action and Vector2D

--- nocturne/python/vector_2d.cc
+++ nocturne/python/vector_2d.cc
@@ -12,6 +12,24 @@ namespace py = pybind11;
 
 namespace nocturne {
 
+namespace {
+
+py::array_t<float> Numpy(const geometry::Vector2D& vec) {
+  py::array_t<float> arr(2);
+  float* arr_data = arr.mutable_data();
+  arr_data[0] = vec.x();
+  arr_data[1] = vec.y();
+  return arr;
+}
+
+geometry::Vector2D FromNumpy(const py::array_t<float>& arr) {
+  assert(arr.size() == 2);
+  const float* arr_data = arr.data();
+  return geometry::Vector2D(arr_data[0], arr_data[1]);
+}
+
+}  // namespace
+
 void DefineVector2D(py::module& m) {
   py::class_<geometry::Vector2D>(m, "Vector2D")
       .def(py::init<float, float>(), py::arg("x") = 0.0, py::arg("y") = 0.0)
@@ -44,13 +62,11 @@ void DefineVector2D(py::module& m) {
       .def("norm", &geometry::Vector2D::Norm, py::arg("p") = 2)
       .def("angle", &geometry::Vector2D::Angle)
       .def("rotate", &geometry::Vector2D::Rotate)
-      .def("numpy", [](const geometry::Vector2D& vec) {
-        py::array_t<float> arr(2);
-        float* arr_data = arr.mutable_data();
-        arr_data[0] = vec.x();
-        arr_data[1] = vec.y();
-        return arr;
-      });
+      .def("numpy", &Numpy)
+      .def_static("from_numpy", &FromNumpy)
+      .def(py::pickle(
+          [](const geometry::Vector2D& vec) { return Numpy(vec); },
+          [](const py::array_t<float>& arr) { return FromNumpy(arr); }));
 }
 
 }  // namespace nocturne
