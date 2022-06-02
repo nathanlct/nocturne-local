switch to fixed standard deviation, make controlled vehicles between train and test match

nocturne/utils/imitation_learning/model.py
@@ -20,7 +20,7 @@ class ImitationAgent(nn.Module):
     def _build_model(self):
         """Build agent MLP that outputs an action mean and variance from a state input."""
         if self.hidden_layers is None or len(self.hidden_layers) == 0:
-            self.nn = nn.Linear(self.n_states, 2 * self.n_actions)
+            self.nn = nn.Linear(self.n_states, self.n_actions)
         else:
             self.nn = nn.Sequential(
                 nn.Linear(self.n_states, self.hidden_layers[0]),
@@ -32,14 +32,15 @@ class ImitationAgent(nn.Module):
                     )
                     for i in range(len(self.hidden_layers) - 1)
                 ],
-                nn.Linear(self.hidden_layers[-1], 2 * self.n_actions),
+                nn.Linear(self.hidden_layers[-1], self.n_actions),
             )
 
     def dist(self, x):
         """Construct a distirbution from tensor x."""
         x_out = self.nn(x)
+        std = torch.ones_like(x_out) * 0.1
         m = MultivariateNormal(x_out[:, 0:self.n_actions],
-                               torch.diag_embed(torch.exp(x_out[:, self.n_actions:])))
+                               torch.diag_embed(std))
         return m
 
     def forward(self, x, deterministic=False):

nocturne/utils/imitation_learning/replay_video.py
@@ -35,6 +35,8 @@ if __name__ == '__main__':
             file for file in Path(data_path).iterdir()
             if 'tfrecord' in file.stem
         ]
+    files = files[:100]
+    np.random.shuffle(files)
     for traj_path in files:
         traj_path = str(traj_path)
         sim = Simulation(scenario_path=os.path.join(data_path, str(traj_path)))
@@ -66,7 +68,7 @@ if __name__ == '__main__':
                 state_size = model.n_states // model.n_stack
                 collections_dict = defaultdict(lambda: deque([np.zeros(state_size) for i in range(model.n_stack)], model.n_stack))
                 for i in range(model.n_stack):
-                    for veh in scenario.getVehicles():
+                    for veh in objects_of_interest:
                         # TODO(eugenevinitsky) remove the 100.0
                         collections_dict[veh.getID()].append(np.concatenate(
                             (np.array(scenario.ego_state(veh), copy=False),
@@ -84,7 +86,7 @@ if __name__ == '__main__':
                             padding=50.0,
                             )
                     frames.append(img)
-                    for veh in scenario.getVehicles():
+                    for veh in objects_of_interest:
                         veh_state = np.concatenate(
                             (np.array(scenario.ego_state(veh), copy=False),
                              np.array(scenario.flattened_visible_state(
@@ -108,7 +110,7 @@ if __name__ == '__main__':
                 print(f'> {mp4_name}')
 
         # stack the movies side by side
-        output_name = traj_path.split('.')[0]
+        output_name = traj_path.split('.')[0].split('/')[-1]
         output_path = f'{output_name}_output.mp4'
         ffmpeg_command = f'ffmpeg -y -i {output_str}_true_rollout.mp4 -i {output_str}_policy_rollout.mp4 -filter_complex hstack {output_path}'
         print(ffmpeg_command)

nocturne/utils/imitation_learning/train.py
@@ -59,6 +59,19 @@ if __name__ == '__main__':
     args = parse_args()
 
     # create dataset (and potentially precompute data)
+    cfg = {
+        'data_path': args.path,
+        'precompute_dataset': args.precompute,
+        'n_cpus': args.n_cpus,
+        'file_limit': args.file_limit,
+        'sample_limit': args.sample_limit,
+        'shuffle': True,
+        'n_stack_input': args.n_stack_input,
+        'pos_grid': [-0.5, 0.5],
+        'pos_grid_size': 25,
+        'heading_grid': [-0.3, 0.3],
+        'heading_grid_size': 25,
+    }
     dataset = WaymoDataset({
         'data_path': args.path,
         'precompute_dataset': args.precompute,
@@ -67,6 +80,10 @@ if __name__ == '__main__':
         'sample_limit': args.sample_limit,
         'shuffle': True,
         'n_stack_input': args.n_stack_input,
+        'pos_grid': [-0.5, 0.5],
+        'pos_grid_size': 25,
+        'heading_grid': [-0.3, 0.3],
+        'heading_grid_size': 25,
     })
 
     # create dataloader
@@ -114,7 +131,7 @@ if __name__ == '__main__':
             loss = -dist.log_prob(expert_actions).mean()
             optimizer.zero_grad()
             loss.backward()
-            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 5.0)
             optimizer.step()
             losses.append(loss.item())
             l2_norms.append(np.mean(np.linalg.norm(expert_actions.detach().cpu().numpy() - dist.mean.detach().cpu().numpy(), axis=1)))