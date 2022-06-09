8b57b5328c129ea9d91b849240ce57bb5cf84c26
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2022-06-08 22:34:00 -0700

    add vectorization

--- nocturne/utils/eval/average_displacement.py
+++ nocturne/utils/eval/average_displacement.py
@@ -54,7 +54,7 @@ def _average_displacement_impl(arg):
         veh for veh in vehicles_model if veh in objects_model
     ]
     random.shuffle(controlled_vehicles)
-    controlled_vehicles = controlled_vehicles[:2]
+    # controlled_vehicles = controlled_vehicles[:2]
 
     # warmup to build up state stacking
     for i in range(n_stacked_states - 1):
@@ -74,6 +74,7 @@ def _average_displacement_impl(arg):
         veh.expert_control = False
 
     avg_displacements = []
+    final_displacements = [0 for _ in controlled_vehicles]
     collisions = [False for _ in controlled_vehicles]
     goal_achieved = [False for _ in controlled_vehicles]
     for i in range(SIM_N_STEPS - n_stacked_states):
@@ -83,6 +84,7 @@ def _average_displacement_impl(arg):
             else:
                 veh.expert_control = False
         # set model actions
+        all_states = []
         for veh in controlled_vehicles:
             # get vehicle state
             state = np.concatenate(
@@ -94,14 +96,20 @@ def _average_displacement_impl(arg):
             state_dict[veh.getID()] = np.roll(state_dict[veh.getID()],
                                               len(state))
             state_dict[veh.getID()][:len(state)] = state
-            # compute vehicle action
-            action = model(torch.unsqueeze(
-                torch.as_tensor(state_dict[veh.getID()], dtype=torch.float32),
-                0),
-                           deterministic=True)
-            # set vehicle action
-            veh.acceleration = action[0].cpu().numpy()[0]
-            veh.steering = action[1].cpu().numpy()[0]
+            all_states.append(state_dict[veh.getID()])
+        all_states = torch.as_tensor(np.array(all_states), dtype=torch.float32)
+
+        # compute vehicle actions
+        all_actions = model(all_states, deterministic=True
+                            )  # /!\ this returns an array (2,n) and not (n,2)
+        accel_actions = all_actions[0].cpu().numpy()
+        steering_actions = all_actions[1].cpu().numpy()
+        # set vehicles actions
+        for veh, accel_action, steering_action in zip(controlled_vehicles,
+                                                      accel_actions,
+                                                      steering_actions):
+            veh.acceleration = accel_action
+            veh.steering = steering_action
 
         # step simulations
         sim_expert.step(dt)
@@ -122,8 +130,9 @@ def _average_displacement_impl(arg):
             model_pos = veh.position
             pos_diff = (model_pos - expert_pos).norm()
             displacements.append(pos_diff)
+            final_displacements[i] = pos_diff
             # a collision with another a vehicle
-            if veh.collided and int(veh.collision_type) == 1:
+            if veh.collided:  #and int(veh.collision_type) == 1:
                 collisions[i] = True
             if (veh.position - veh.target_position).norm() < GOAL_TOLERANCE:
                 goal_achieved[i] = True
@@ -136,17 +145,20 @@ def _average_displacement_impl(arg):
     # average displacements over all time steps
     avg_displacement = np.mean(
         avg_displacements) if len(avg_displacements) > 0 else np.nan
+    final_displacement = np.mean(
+        final_displacements) if len(final_displacements) > 0 else np.nan
     avg_collisions = np.mean(collisions) if len(collisions) > 0 else np.nan
     avg_goals = np.mean(goal_achieved) if len(goal_achieved) > 0 else np.nan
     print('displacements', avg_displacement)
+    print('final_displacement', final_displacement)
     print('collisions', avg_collisions)
     print('goal_rate', avg_goals)
-    return avg_displacement, avg_collisions, avg_goals
+    return avg_displacement, final_displacement, avg_collisions, avg_goals
 
 
 def compute_average_displacement(trajectories_dir, model, configs):
     """Compute average displacement error between a model and the ground truth."""
-    NUM_FILES = 280
+    NUM_FILES = 200
     # get trajectories paths
     if isinstance(trajectories_dir, str):
         # if trajectories_dir is a string, treat it as the path to a directory of trajectories
@@ -164,21 +176,24 @@ def compute_average_displacement(trajectories_dir, model, configs):
             pool.map(_average_displacement_impl,
                      zip(trajectories_paths, repeat(model), repeat(configs))))
         average_displacements = np.array(result)[:, 0]
-        average_collisions = np.array(result)[:, 1]
-        average_goals = np.array(result)[:, 2]
-        print(average_displacements, average_collisions, average_goals)
+        final_displacements = np.array(result)[:, 1]
+        average_collisions = np.array(result)[:, 2]
+        average_goals = np.array(result)[:, 3]
+        print(average_displacements, final_displacements, average_collisions,
+              average_goals)
 
     return [
         np.mean(average_displacements[~np.isnan(average_displacements)]),
-        np.std(average_displacements[~np.isnan(average_displacements)]) /
-        np.sqrt(NUM_FILES)
+        np.std(average_displacements[~np.isnan(average_displacements)])
+    ], [
+        np.mean(final_displacements[~np.isnan(final_displacements)]),
+        np.std(final_displacements[~np.isnan(final_displacements)])
     ], [
         np.mean(average_collisions[~np.isnan(average_collisions)]),
-        np.std(average_collisions[~np.isnan(average_displacements)]) /
-        np.sqrt(NUM_FILES)
+        np.std(average_collisions[~np.isnan(average_displacements)])
     ], [
         np.mean(average_goals[~np.isnan(average_goals)]),
-        np.std(average_goals[~np.isnan(average_goals)]) / np.sqrt(NUM_FILES)
+        np.std(average_goals[~np.isnan(average_goals)])
     ]
 
 
@@ -190,17 +205,19 @@ if __name__ == '__main__':
     ).to('cpu')
     model.actions_grids = [x.to('cpu') for x in model.actions_grids]
     model.eval()
+    model.nn[0].eval()
     with open(
             '/checkpoint/eugenevinitsky/nocturne/test/2022.06.05/test/14.23.17/++device=cuda,++file_limit=1000/train_logs/2022_06_05_14_23_23/configs.json',
             'r') as fp:
         configs = json.load(fp)
         configs['device'] = 'cpu'
     with torch.no_grad():
-        ade, collisions, goals = compute_average_displacement(
+        ade, fde, collisions, goals = compute_average_displacement(
             '/checkpoint/eugenevinitsky/waymo_open/motion_v1p1/uncompressed/scenario/formatted_json_v2_no_tl_valid',
             model=model,
             configs=configs)
     print(f'Average Displacement Error: {ade[0]:.3f} ± {ade[1]:.3f} meters')
+    print(f'Final Displacement Error: {fde[0]:.3f} ± {fde[1]:.3f} meters')
     print(f'Average Collisions: {collisions[0]:.3f} ± {collisions[1]:.3f}\%')
     print(
         f'Average Success at getting to goal: {goals[0]:.3f} ± {goals[1]:.3f}\%'
